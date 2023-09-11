04e0082fddfe15acdf432e22ace2c0c87a2283f0
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-09-11 09:15:16 +0200

    second

--- Interaktiv_berättelse/Interaktiv_berättelse/Program.cs
+++ Interaktiv_berättelse/Interaktiv_berättelse/Program.cs
@@ -1,9 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Vill du gå åt höger eller vänster?");
 
-string question1 = Console.ReadLine();
+string choice1 = Console.ReadLine();
 
-if (i > 4)
+if (choice1 == "höger")                             //första frågan
 {
-    
+    Console.WriteLine("Du kommer till en gruva. Vill du gå in i gruvan eller gå vidare?");
 }
+else if (choice1 == "vänster")
+{
+    Console.WriteLine("Vägen tog slut. Vill du gå tillbaka?");
+}
+else
+{
+    Console.WriteLine("Försök igen");
+}
+
+string choice2 = Console.ReadLine();
+
+if (choice2 == "gå in i gruvan")                  //till höger - andra frågan
+
+{
+    Console.WriteLine("Hoppsan! Du blev attackerad av fladdermöss. Slut");
+}
+else if (choice2 == "gå vidare")
+{
+    Console.WriteLine("Du går förbi gruvan och kommer fram till en mur. Vill du stanna vid muren eller gå vidare?");
+}
+else if (choice2 == "gå tillbaka")
+    {
+        Console.WriteLine("Du är tillbaka vid korsningen. Slut");
+    }
+else if (choice2 == "nej")
+{
+    Console.WriteLine("Du har inget val. Slut");
+}
+else
+{
+    Console.WriteLine("Försök igen");
+}
+
+string choice3 = Console.ReadLine();              //till höger - tredje frågan
+
+if (choice3 == "stanna vid muren")
+{
+    Console.WriteLine("Någon kastar dig över muren och du slår i huvudet mot en sten. Slut");
+}
+else if (choice3 == "gå vidare")
+{
+    Console.WriteLine("Du är tillbaka där du började. Slut");
+}
+else
+{
+    Console.WriteLine("Försök igen");
+}
+
+if (choice1 == "vänster")
+{
+    Console.WriteLine("Vägen tog slut. Vill du gå tillbaka?");
+}
+
+
+
+Console.ReadLine();
