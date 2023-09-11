dc56af89bc9f2285ba288f7f40d87ae47f0f681b
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-09-11 08:30:18 +0200

    first

---
+++ Interaktiv_berättelse/Interaktiv_berättelse/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vill du gå åt höger eller vänster?");
+
+string question1 = Console.ReadLine();
+
+if (i > 4)
+{
+    
+}
