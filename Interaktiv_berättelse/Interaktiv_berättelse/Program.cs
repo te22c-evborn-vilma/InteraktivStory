db2fe80384187d61a1a272a76df2d2f8bcd2bcd2
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-09-11 09:21:34 +0200

    final

--- Interaktiv_berättelse/Interaktiv_berättelse/Program.cs
+++ Interaktiv_berättelse/Interaktiv_berättelse/Program.cs
@@ -11,55 +11,47 @@ else if (choice1 == "vänster")
 {
     Console.WriteLine("Vägen tog slut. Vill du gå tillbaka?");
 }
-else
-{
-    Console.WriteLine("Försök igen");
-}
+// else
+// {
+    // Console.WriteLine("Försök igen");
+// }
+
 
 string choice2 = Console.ReadLine();
-
-if (choice2 == "gå in i gruvan")                  //till höger - andra frågan
-
+if (choice2 == "gå in i gruvan")                  //till höger - gå in i gruvan
 {
     Console.WriteLine("Hoppsan! Du blev attackerad av fladdermöss. Slut");
 }
-else if (choice2 == "gå vidare")
+else if (choice2 == "gå vidare")                  //till höger - gå vidare
 {
     Console.WriteLine("Du går förbi gruvan och kommer fram till en mur. Vill du stanna vid muren eller gå vidare?");
 }
-else if (choice2 == "gå tillbaka")
+else if (choice2 == "ja")                         //till vänster - ja
     {
         Console.WriteLine("Du är tillbaka vid korsningen. Slut");
     }
-else if (choice2 == "nej")
+else if (choice2 == "nej")                        //till vänster - nej
 {
     Console.WriteLine("Du har inget val. Slut");
 }
-else
-{
-    Console.WriteLine("Försök igen");
-}
+// else
+// {
+    // Console.WriteLine("Försök igen");
+// }
 
-string choice3 = Console.ReadLine();              //till höger - tredje frågan
 
-if (choice3 == "stanna vid muren")
+string choice3 = Console.ReadLine();
+if (choice3 == "stanna vid muren")                 //till höger - stanna vid muren
 {
     Console.WriteLine("Någon kastar dig över muren och du slår i huvudet mot en sten. Slut");
 }
-else if (choice3 == "gå vidare")
+else if (choice3 == "gå vidare")                  //till höger - gå vidare
 {
     Console.WriteLine("Du är tillbaka där du började. Slut");
 }
-else
-{
-    Console.WriteLine("Försök igen");
-}
-
-if (choice1 == "vänster")
-{
-    Console.WriteLine("Vägen tog slut. Vill du gå tillbaka?");
-}
-
-
+// else
+// {
+    // Console.WriteLine("Försök igen");
+// }
 
 Console.ReadLine();
